909901e1b33fe89c8a7200484fb0e131e8dd4bd1
kimkiyong <fenderst0612@gmail.com>
2024-03-20 22:30:35 +0900

    feat: refactor repository functions and update models

--- internal/repository/article.go
+++ internal/repository/article.go
@@ -3,47 +3,99 @@ package repository
 import (
 	"context"
 
+	"github.com/cockroachdb/errors"
 	"github.com/kyong0612/my-go-clean-architecture/domain"
 	"github.com/kyong0612/my-go-clean-architecture/internal/repository/postgres"
 )
 
-func articleToModel(article postgres.Article, author postgres.Author) domain.Article {
-	return domain.Article{
+func articleToModel(article postgres.Article, author *postgres.Author) *domain.Article {
+	result := domain.Article{
 		ID:        article.ID,
 		Title:     article.Title,
 		Content:   article.Content,
-		Author:    authorToModel(author),
-		CreatedAt: article.CreatedAt.Time,
-		UpdatedAt: article.UpdatedAt.Time,
+		CreatedAt: article.CreatedAt,
+		UpdatedAt: article.UpdatedAt,
 	}
+
+	if author != nil {
+		result.Author = authorToModel(*author)
+	}
+
+	return &result
 }
 
-func (r *Repository) FetchArticles(ctx context.Context, cursor string, num int32) (res []domain.Article, nextCursor string, err error) {
-	// TODO: implement the code
-	return nil, "", nil
+func (r *Repository) FetchArticles(ctx context.Context, cursor string, num int32) (res []*domain.Article, nextCursor string, err error) {
+	sqlCursor, err := DecodeCursor(cursor)
+	if err != nil {
+		return nil, "", errors.Wrap(err, "failed to decode cursor")
+	}
+
+	articles, err := r.db.ListArticles(ctx, postgres.ListArticlesParams{
+		Cursor: sqlCursor,
+		Limit:  int64(num),
+	})
+	if err != nil {
+		return nil, "", errors.Wrap(err, "failed to fetch articles")
+	}
+
+	result := make([]*domain.Article, len(articles))
+	for _, article := range articles {
+		result = append(result, articleToModel(article, nil))
+	}
+
+	return result, EncodeCursor(result[len(result)-1].CreatedAt), nil
 }
 
-func (r *Repository) GetArticleByID(ctx context.Context, id int32) (domain.Article, error) {
-	// TODO: implement the code
-	return domain.Article{}, nil
+func (r *Repository) GetArticleByID(ctx context.Context, id int32) (*domain.Article, error) {
+	article, err := r.db.GetArticleByID(ctx, id)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get article by id")
+	}
+	return articleToModel(article, nil), nil
 }
 
-func (r *Repository) GetArticleByTitle(ctx context.Context, title string) (domain.Article, error) {
-	// TODO: implement the code
-	return domain.Article{}, nil
+func (r *Repository) GetArticleByTitle(ctx context.Context, title string) (*domain.Article, error) {
+	article, err := r.db.GetArticleByTitle(ctx, title)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get article by title")
+	}
+
+	return articleToModel(article, nil), nil
 }
 
-func (r *Repository) UpdateArticle(ctx context.Context, ar *domain.Article) error {
-	// TODO: implement the code
+func (r *Repository) UpdateArticle(ctx context.Context, ar domain.Article) error {
+	if err := r.db.UpdateArticle(ctx, postgres.UpdateArticleParams{
+		ID:      ar.ID,
+		Title:   &ar.Title,
+		Content: &ar.Content,
+	}); err != nil {
+		return errors.Wrap(err, "failed to update article")
+	}
+
 	return nil
 }
 
-func (r *Repository) StoreArticle(ctx context.Context, a *domain.Article) error {
-	// TODO: implement the code
+func (r *Repository) StoreArticle(ctx context.Context, a domain.Article) error {
+	param := postgres.CreateArticleParams{
+		Title:   a.Title,
+		Content: a.Content,
+	}
+
+	if a.Author != nil {
+		param.AuthorID = &a.Author.ID
+	}
+
+	if err := r.db.CreateArticle(ctx, param); err != nil {
+		return errors.Wrap(err, "failed to store article")
+	}
+
 	return nil
 }
 
 func (r *Repository) DeleteArticle(ctx context.Context, id int32) error {
-	// TODO: implement the code
+	if err := r.db.DeleteArticle(ctx, id); err != nil {
+		return errors.Wrap(err, "failed to delete article")
+	}
+
 	return nil
 }
--- internal/repository/author.go
+++ internal/repository/author.go
@@ -7,19 +7,19 @@ import (
 	"github.com/kyong0612/my-go-clean-architecture/internal/repository/postgres"
 )
 
-func authorToModel(author postgres.Author) domain.Author {
-	return domain.Author{
+func authorToModel(author postgres.Author) *domain.Author {
+	return &domain.Author{
 		ID:        author.ID,
 		Name:      author.Name,
-		CreatedAt: author.CreatedAt.Time,
-		UpdatedAt: author.UpdatedAt.Time,
+		CreatedAt: author.CreatedAt,
+		UpdatedAt: author.UpdatedAt,
 	}
 }
 
-func (r *Repository) GetAuthorByID(ctx context.Context, id int32) (domain.Author, error) {
+func (r *Repository) GetAuthorByID(ctx context.Context, id int32) (*domain.Author, error) {
 	author, err := r.db.GetAuthorByID(ctx, id)
 	if err != nil {
-		return domain.Author{}, err
+		return nil, err
 	}
 	return authorToModel(author), nil
 }

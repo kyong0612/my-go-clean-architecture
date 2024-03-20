57b896b14324196b47f508c8f536259868461df0
kimkiyong <fenderst0612@gmail.com>
2024-03-20 22:30:23 +0900

    feat: update Author field in Article struct to use a pointer

--- domain/article.go
+++ domain/article.go
@@ -9,7 +9,7 @@ type Article struct {
 	ID        int32     `json:"id"`
 	Title     string    `json:"title"      validate:"required"`
 	Content   string    `json:"content"    validate:"required"`
-	Author    Author    `json:"author"`
+	Author    *Author   `json:"author"`
 	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
 }
